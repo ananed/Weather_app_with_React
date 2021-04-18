197e3210903782b30e236060e5cc70365bdddddf
Anita <ana@codewithdragos.com>
2021-04-18 20:44:34 +0200

    changes

--- App.js
+++ App.js
@@ -1,89 +1,80 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
+
+const dateBuilder = (d) => {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+};
 
 function App() {
   // defino mi constante de los datos
-  const [weather, setWeather] = useState({});
-  const [query, setQuery] = useState("");
-  const [url, setUrl] = useState(
-    `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=6e7849af5ea991b87272b3a72ea28802`
-  );
+  const [weather, setWeather] = useState(null);
+  const [query, setQuery] = useState("Madrid");
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=6e7849af5ea991b87272b3a72ea28802`;
+
   //saco la fecha
-  const dateBuilder = (d) => {
-    let months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`;
-  };
-
-  useEffect(() => {
-    async function fetchWeather() {
-      const resp = await fetch(url);
-      const json = await resp.json();
-      setWeather(json.results);
-    }
-
-    fetchWeather();
-  }, [url]);
+  async function fetchWeather() {
+    const response = await fetch(url);
+    const data = await response.json();
+    setWeather(data);
+  }
 
   //los he guardado en weather
   return (
     <div className="App">
       <main>
         <div className="search-box">
-          <div
-            onSubmit={() =>
-              setUrl(
-                `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=6e7849af5ea991b87272b3a72ea28802`
-              )
-            }
-          >
+          <div>
             <input
-              label="Name"
+              label="name"
               value={query}
               onChange={(e) => setQuery(e.target.value)}
             />
-            <button>Search</button>
-         </div>
+            <button onClick={() => fetchWeather(url)}>Search</button>
+          </div>
         </div>
-        <div>
-          <div className="location-box">
-            <div className="location">
-              {weather.name}, {weather.sys.country}
+        {weather && (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
             </div>
-            <div className="date">{dateBuilder(new Date())}</div>
+            <div className="weather-box">
+              <div className="temp">{weather.temp}</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
           </div>
-          <div className="weather-box">
-            <div className="temp">{weather.temp}</div>
-            <div className="weather">{weather.weather[0].main}</div>
-          </div>
-        </div>
+        )}
       </main>
     </div>
   );
